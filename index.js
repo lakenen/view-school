e0d8da0343a60a80a91458587b96026a16a57366
Cameron Lakenen <lakenen@box.com>
2014-08-11 20:13:23 -0700

    message on exit

--- index.js
+++ index.js
@@ -17,4 +17,17 @@ require('portfinder').getPort(function (err, port) {
       }
     , mainBundler: require('./bundler')
   })
+})
+
+process.on('exit', exit)
+process.on('SIGINT', function () {
+  process.exit()
 });
+
+function exit() {
+  console.log(
+    '\nSad to see you go!\n\n' +
+    'To run the workshop again, type `view-school` in this directory.\n' +
+    'Don\'t worry, your answers will not be overwritten!'
+  )
+}
