7d234a683a67e4e6464307296299846b64d38e14
Cameron Lakenen <lakenen@box.com>
2015-04-17 12:38:50 -0700

    Fix viewer css + brfs bug

--- bundler.js
+++ bundler.js
@@ -7,6 +7,6 @@ module.exports = function (b) {
     , bundler: function (fn) { fn(b) }
   })
   b.transform(require('envify/custom')({ BVBB_URL: process.env.BVBB_URL }))
-  b.transform(require.resolve('cssify'), { global: true })
+  b.transform(require.resolve('cssify'))
   b.add(require.resolve(__dirname + '/browser'))
 }
